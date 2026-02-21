6cb9c9de6c9d03e175745bc7845703cec3dbfdba
Daria Golovashova <your_email>
2026-02-21 18:35:18 +0100

    add task 8

---
+++ src/task-8.ts
@@ -0,0 +1,32 @@
+// 1. Створіть функцію getPermissions, яка приймає параметр role типу Role.
+
+// 2. Функція має повертати масив рядків, які описують права доступу для кожної ролі:
+
+// Admin має права: "create", "read", "update", "delete"
+// User має права: "read", "update"
+// Guest має лише право: "read"
+// 3. Типізуйте параметр і тип повернення функції getPermissions.
+
+// 4. Перевірте, що TypeScript не дозволяє передати в getPermissions значення, якого немає в Role.
+
+enum Role {
+  Admin,
+  User,
+  Guest
+}
+
+function getPermissions(role: Role) {
+    switch (role) {
+        case Role.Admin:
+            return ["create", "read", "update", "delete"];
+        case Role.Guest:
+            return["read", "update"];
+        case Role.User:
+            return ["read"];
+            
+        default: return[];
+     }
+    
+ } 
+
+console.log(getPermissions(Role.Admin));
